c93d19b3edf9dffaad32c2fd6f363a8ce66ed85a
Aleksei7n <aleksey.semyan@gmail.com>
2025-02-12 19:32:28 +0300

    Add new JS files for tasks

---
+++ task_score.js
@@ -0,0 +1,18 @@
+- введение
+- git 
+- js 
+
+let studentName = "Oksana"
+let studentSurname = "Melnikova"
+
+let shorName = studentName.slice(0, 1)
+
+
+    let introModuleScore = 50
+    let gitModuleScore = 70
+    let jsModuleScore = 3
+
+    let averageScore = (introModuleScore+gitModuleScore+jsModuleScore)/3;
+
+    console.log(`The averageScore score for ${shorName} ${studentSurname} is ${averageScore}`);
+
